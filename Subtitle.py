269a838f4d65439d72b155302f9faa65e16e5844
sdboer78 <sander@deboernet.eu>
2018-03-07 20:24:12 +0100

    Update Subtitle.py

--- Subtitle.py
+++ Subtitle.py
@@ -11,7 +11,7 @@ class Application():
     screen = None
     windowHeight = 0
     windowWidth = 0
-        
+
     # Store the threaded class for ProPresenter
     ProPresenter = None
 
@@ -19,65 +19,66 @@ class Application():
     ProP_IPAddress = None
     ProP_IPPort = None
     ProP_Password = None
-    
+
     # Do we need to attempt a reconnection?
     tryReconnect = False
     disconnectTime = 0
 
     data = None
-    
+
     subtitle = None
-    
+
     def __init__(self):
 
         pygame.init()
         pygame.mouse.set_visible(False)
-        
+
         screenInfo = pygame.display.Info()
-               
+
         self.screen = pygame.display.set_mode((screenInfo.current_w, screenInfo.current_h), pygame.FULLSCREEN)
-        
+        #self.screen = pygame.display.set_mode((1800, 400))
+
         pygame.display.flip()
-        
+
         self.windowWidth = pygame.display.get_surface().get_width()
         self.windowHeight = pygame.display.get_surface().get_height()
-        
+
         try:
             ConfigData_Filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "subtitle-config.json")
             ConfigData_JSON = open(ConfigData_Filename).read()
             ConfigData = json.loads(ConfigData_JSON)
-    
+
         except Exception, e:
             print
             print "##############################################"
             print "EXCEPTION: Cannot load and parse subtitle-config.json File: "
             print e
             print "##############################################"
-            print        
+            print
             exit()
-        
+
         try:
             self.ProP_IPAddress = ConfigData['IPAddress']
             self.ProP_IPPort = int(ConfigData['IPPort'])
             self.ProP_Password = ConfigData['Password']
 
-            self.subtitle = ConfigData['Subtitle']             
-                
+            self.subtitle = ConfigData['Subtitle']
+
         except Exception, e:
             print
             print "##############################################"
             print "EXCEPTION: Config file is missing a setting"
             print e
             print "##############################################"
-            print            
+            print
             exit()
-       
+
         self.update("Waiting for data ....")
-        
+
         self.connect()
-        
+
         clock = pygame.time.Clock()
-        done = False         
+        done = False
         while not done:
             self.reconnect_tick()
 
@@ -90,9 +91,9 @@ class Application():
                     self.ProPresenter.stop()
                     pygame.quit()
                     sys.exit()
-            
+
             clock.tick(1)
- 
+
     def connect(self):
         # Connect to ProPresenter and setup the necessary callbacks
         self.tryReconnect = False
@@ -110,37 +111,40 @@ class Application():
 
     def connectFailed(self, error):
         self.tryReconnect = True
-        
+
         if self.disconnectTime == 0:
             self.disconnectTime = time.time()
-        
+
         print "ProPresenter Connect Failed", error
-    
+
     def disconnected(self, error):
         self.tryReconnect = True
-        
+
         if self.disconnectTime == 0:
             self.disconnectTime = time.time()
-        
-        self.update("")
-        
+
         print "ProPresenter Disconnected", error
 
     def reconnect_tick(self):
+        print "reconnect_tick"
         if self.tryReconnect and self.disconnectTime < time.time() - 5:
             print "Attempting to reconnect to ProPresenter"
             self.connect()
-            
+
     def update(self, data):
         print "Updating... "
 
-        if not isinstance(data, basestring): 
-            data = data['text'].encode('utf-8').rstrip().strip()
-            
-        if (self.data != data):
-            self.data = data
+        output = ""
+        if not isinstance(data, basestring):
+            if data['text'] is not None:
+                output = data['text'].encode('latin-1', 'ignore').rstrip().strip()
+        else:
+                output = data
+
+        if (self.data != output):
+            self.data = output
             self.screen.fill(tuple(self.subtitle['bgcolor']))
-        
+
             if (self.subtitle['align'] == 'left'):
                 bottom = self.windowHeight - self.subtitle['marginBottom']
                 midtop = None
@@ -149,23 +153,25 @@ class Application():
                 bottom = self.windowHeight - self.subtitle['marginBottom']
                 midtop = (self.windowWidth / 2, 0)
                 left = None
-            
-            ptext.draw(data, 
-                bottom      = bottom, 
+
+            ptext.draw(output,
+                bottom      = bottom,
                 midtop      = midtop,
-                left        = left,                
-                align       = str(self.subtitle['align']), 
-                sysfontname = str(self.subtitle['sysfontname']), 
-                fontsize    = float(self.subtitle['fontsize']), 
-                color       = tuple(self.subtitle['color']), 
-                shadow      = tuple(self.subtitle['shadow']), 
-                scolor      = tuple(self.subtitle['scolor']), 
-                owidth      = float(self.subtitle['owidth']), 
+                left        = left,
+                align       = str(self.subtitle['align']),
+                sysfontname = str(self.subtitle['sysfontname']),
+                fontsize    = float(self.subtitle['fontsize']),
+                color       = tuple(self.subtitle['color']),
+                shadow      = tuple(self.subtitle['shadow']),
+                scolor      = tuple(self.subtitle['scolor']),
+                owidth      = float(self.subtitle['owidth']),
                 ocolor      = tuple(self.subtitle['ocolor']))
-            
+
             pygame.display.flip()
-            
-            print data
- 
+
+            print output
+
+        sys.stdout.flush()
+
 if __name__ == '__main__':
         app = Application()
